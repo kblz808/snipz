f157e050fa22ea86a1afa12a3cb40b7c0fa652f6
kaleab <808kaleabkzin@gmail.com>
2025-02-20 19:40:02 +0300

    snippet repository implementation

--- internal/storage/repository/snippet.repository.go
+++ internal/storage/repository/snippet.repository.go
@@ -1,6 +1,14 @@
 package repository
 
-import "time"
+import (
+	"context"
+	"errors"
+	"snipz/internal/storage"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
+)
 
 type Visibility string
 
@@ -33,4 +41,159 @@ type Snippet struct {
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type SnippetRepository struct {
+	db *storage.DB
+}
+
+func NewSnippetRepository(db *storage.DB) *SnippetRepository {
+	return &SnippetRepository{db}
+}
+
+func (repo *SnippetRepository) CreateSnippet(ctx context.Context, snippet *Snippet) (*Snippet, error) {
+	query := repo.db.QueryBuilder.Insert("snippets").
+		Columns("title", "content", "language", "user_id", "expires_at", "visibility", "is_encrypted", "password_hash").
+		Values(snippet.Title, snippet.Content, snippet.Language, snippet.UserID, snippet.ExpiresAt, snippet.Visibility, snippet.IsEncrypted, snippet.PasswordHash).
+		Suffix("RETURNING *")
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	err = repo.db.QueryRow(ctx, sql, args...).Scan(
+		&snippet.ID,
+		&snippet.Title,
+		&snippet.Content,
+		&snippet.Language,
+		&snippet.UserID,
+		&snippet.ExpiresAt,
+		&snippet.Visibility,
+		&snippet.IsEncrypted,
+		&snippet.PasswordHash,
+		&snippet.ViewCount,
+		&snippet.Tags,
+		&snippet.StarsCount,
+		&snippet.CreatedAt,
+	)
+	if err != nil {
+		if errCode := repo.db.ErrorCode(err); errCode == "23505" {
+			// TODO: replace with domain specific error
+			return nil, errors.New("conflicting data")
+		}
+		return nil, err
+	}
+
+	return snippet, nil
+}
+
+func (repo *SnippetRepository) GetSnippetByID(ctx context.Context, id uint64) (*Snippet, error) {
+	var snippet Snippet
+
+	query := repo.db.QueryBuilder.Select("*").
+		From("snippets").
+		Where(sq.Eq{"id": id}).
+		Limit(1)
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	err = repo.db.QueryRow(ctx, sql, args...).Scan(
+		&snippet.ID,
+		&snippet.Title,
+		&snippet.Content,
+		&snippet.Language,
+		&snippet.UserID,
+		&snippet.ExpiresAt,
+		&snippet.Visibility,
+		&snippet.IsEncrypted,
+		&snippet.PasswordHash,
+		&snippet.ViewCount,
+		&snippet.Tags,
+		&snippet.StarsCount,
+		&snippet.CreatedAt,
+	)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			// TODO: replace with domain specific error
+			return nil, errors.New("data not found")
+		}
+		return nil, err
+	}
+
+	return &snippet, nil
+}
+
+func (repo *SnippetRepository) ListSnippets(ctx context.Context, title, language string, skip, limit uint64) ([]Snippet, error) {
+	var snippet Snippet
+	var snippets []Snippet
+
+	query := repo.db.QueryBuilder.Select("*").
+		From("snippets").
+		OrderBy("id").
+		Limit(limit).
+		Offset((skip - 1) * limit)
+
+	if language != "" {
+		query = query.Where(sq.Eq{"language": language})
+	}
+
+	if title != "" {
+		query = query.Where(sq.ILike{"title": "%" + title + "%"})
+	}
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := repo.db.Query(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(
+			&snippet.ID,
+			&snippet.Title,
+			&snippet.Content,
+			&snippet.Language,
+			&snippet.UserID,
+			&snippet.ExpiresAt,
+			&snippet.Visibility,
+			&snippet.IsEncrypted,
+			&snippet.PasswordHash,
+			&snippet.ViewCount,
+			&snippet.Tags,
+			&snippet.StarsCount,
+			&snippet.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, snippet)
+	}
+
+	return snippets, nil
+}
+
+func (repo *SnippetRepository) DeleteSnippet(ctx context.Context, id uint64) error {
+	query := repo.db.QueryBuilder.Delete("snippets").
+		Where(sq.Eq{"id": id})
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = repo.db.Exec(ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/storage/repository/user.repository.go
+++ internal/storage/repository/user.repository.go
@@ -1,6 +1,14 @@
 package repository
 
-import "time"
+import (
+	"context"
+	"errors"
+	"snipz/internal/storage"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
+)
 
 type User struct {
 	ID int64
@@ -10,4 +18,64 @@ type User struct {
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type UserRepository struct {
+	db *storage.DB
+}
+
+func NewUserRepository(db *storage.DB) *UserRepository {
+	return &UserRepository{db}
+}
+
+func (ur *UserRepository) CreateUser(ctx context.Context, user *User) (*User, error) {
+	query := ur.db.QueryBuilder.Insert("users").
+		Columns("username", "hashed_password").
+		Values(user.Username, user.HashedPassword).
+		Suffix("RETURNING *")
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	err = ur.db.QueryRow(ctx, sql, args...).Scan(
+		&user.ID,
+		&user.Username,
+	)
+	if err != nil {
+		if errCode := ur.db.ErrorCode(err); errCode == "23505" {
+			return nil, errors.New("conflicting data")
+		}
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (ur *UserRepository) GetUserByID(ctx context.Context, id uint64) (*User, error) {
+	var user User
+
+	query := ur.db.QueryBuilder.Select("*").
+		From("users").
+		Where(sq.Eq{"id": id}).
+		Limit(1)
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	err = ur.db.QueryRow(ctx, sql, args...).Scan(
+		&user.ID,
+		&user.Username,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, errors.New("data not found")
+		}
+	}
+
+	return &user, nil
 }
